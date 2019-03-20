1aed598fdf05f98e7efffde80ff8303660720502
v627222422 <39487849@users.noreply.github.com>
2019-03-20 15:46:07 +0800

    a

--- src/pages/content/content.js
+++ src/pages/content/content.js
@@ -4,34 +4,52 @@ Page({
   data: {
     showModal: false,
     isShowDis:[false,false],
-    dataList:[
-      {
-        name: '姓名',
-        type: 'text',
-        defaultValue:null,
-        description:'1-4个字符',
-        tip:'真实姓名',
-        require: true,
-        case:null,
-        range:null,
-        subItem:null
-      },
-      {
-        name: '学号',
-        type: 'number',
-        defaultValue: null,
-        description: '30位以内纯数字',
-        tip: '',
-        require: true,
-        case: null,
-        range: null,
-        subItem: null
-      }
-    ],
+    dataList:{},
+    // dataList:[
+    //   {
+    //     name: '姓名',
+    //     type: 'text',
+    //     defaultValue:null,
+    //     description:'1-4个字符',
+    //     tip:'真实姓名',
+    //     require: true,
+    //     case:null,
+    //     range:null,
+    //     subItem:null
+    //   },
+    //   {
+    //     name: '学号',
+    //     type: 'number',
+    //     defaultValue: null,
+    //     description: '30位以内纯数字',
+    //     tip: '',
+    //     require: true,
+    //     case: null,
+    //     range: null,
+    //     subItem: null
+    //   }
+    // ],
     stateList: [false, true]
   },
   
   onLoad: function () {
+    var that=this;
+    wx.request({
+      url: 'https://www.turing-cup.online/voteapp/activity/3',
+      data: {
+        
+      },
+      method:'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        that.setData({ dataList: res.data });
+        console.log(res.data)
+      }
+    })
+    
+    
   },
   addDis(event){
     var s=true;
--- src/pages/list/list.js
+++ src/pages/list/list.js
@@ -1,41 +1,90 @@
-
+// pages/list/list.js
 Page({
-  data: {
-    showModal: false,
-  },
-  onLoad: function () {
-  },
   /**
-   * 弹窗
+   * 页面的初始数据
    */
-  showDialogBtn: function () {
-    this.setData({
-      showModal: true
+  data: {
+    activitylist: [],
+    keyword: '',
+
+  },
+  jumpPage: function () {
+    wx.navigateTo({
+      url: '/pages/content/content',
     })
   },
+
   /**
-   * 弹出框蒙层截断touchmove事件
+   * 生命周期函数--监听页面加载
    */
-  preventTouchMove: function () {
+  getdata: function () {
+    var that = this;
+    wx.request({
+      url: 'https://www.turing-cup.online/voteapp/activity',
+      header: {
+        "Content-Type": "applciation/json"
+      },
+      method: "GET",
+
+      success: function (res) {
+        console.log(res.data);
+        that.setData({ activitylist: res.data });
+      },
+      fail: function (err) { },
+      complete: function () { },
+    })
   },
-  /**
-   * 隐藏模态对话框
-   */
-  hideModal: function () {
-    this.setData({
-      showModal: false
-    });
+
+  onLoad: function (options) {
+    this.getdata();
   },
+
   /**
-   * 对话框取消按钮点击事件
+   * 生命周期函数--监听页面初次渲染完成
    */
-  onCancel: function () {
-    this.hideModal();
+  onReady: function () {
+
   },
+
   /**
-   * 对话框确认按钮点击事件
+   * 生命周期函数--监听页面显示
    */
-  onConfirm: function () {
-    this.hideModal();
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
